dea2ddd1e48f61ebb0af89a55410c56af29734fe
keithbooher <keith.booher@yahoo.com>
2020-02-04 23:54:21 -0600

    fixed non saving surveys

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -8,6 +8,8 @@ const Mailer = require('../services/Mailer')
 const surveyTemplate = require('../services/emailTemplates/surveyTemplate')
 
 const Survey = mongoose.model('surveys')
+const User = mongoose.model('users')
+const db = mongoose.connection;
 
 module.exports = app => {
   app.get('/api/surveys/thanks', (req, res) => {
@@ -15,7 +17,7 @@ module.exports = app => {
   })
   app.post('/api/surveys/webhooks', (req, res) => {
     const p = new Path('/api/surveys/:surveyId/:choice')
-    const events = _.chain(req.body)
+    _.chain(req.body)
       .map(({ email, url }) => {
         const match = p.test(new URL(url).pathname)
         if (match) {
@@ -24,8 +26,26 @@ module.exports = app => {
       })
       .compact()
       .uniqBy('email', 'surveyId')
+      .each(({ surveyId, email, choice }) => {
+        console.log('ID ID ID ID')
+        console.log('ID ID ID ID')
+        console.log(surveyId)
+        // Here we are finding the survey with the corresponding ID
+        // and retrieve a recipient with the right email address and responded value with false
+        // then in the second object we tell mongo to
+        // increment the chosen response by one
+        // and change the recipient.[index] responded value to true
+        Survey.updateOne({
+          _id: surveyId,
+          recipients: {
+            $elemMatch: { email: email, responded: false}
+          }
+        }, {
+          $inc: { [choice]: 1 },
+          $set: { 'recipients.$.responded': true}
+        }).exec()
+      })
       .value()
-    console.log(events)
   })
   app.post('/api/surveys', requireLogin, requireCredits, async (req, res) => {
     // the below translates to const title = req.body.title and so on
@@ -45,13 +65,22 @@ module.exports = app => {
 
     try {
       await mailer.send()
-      await survey.save
+      await survey.save()
       req.user.credits -= 1
       const user = await req.user.save()
 
+      const survey_log = survey
+      console.log('!!!---SURVEY LOG---!!!')
+      console.log(survey_log)
       res.send(user)
     } catch (err) {
       res.status(422).send(err)
     }
+  })
+  app.get('/api/surveys/find_something', (req, res) => {
+    Survey.findById('5e3a5326b352af1cac8b91a0', function (err, data){
+        res.send(data)
+    })
+    
   })
 }
